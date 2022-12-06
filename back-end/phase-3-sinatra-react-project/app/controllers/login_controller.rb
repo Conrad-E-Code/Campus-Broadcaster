022f7e09d360810ee6505b7583da70b69b7513f3
Conrad Etherington <conrad.etherington@gmail.com>
2022-12-05 22:28:28 -0700

    password almost working

---
+++ back-end/phase-3-sinatra-react-project/app/controllers/login_controller.rb
@@ -0,0 +1,14 @@
+class LoginController < ApplicationController
+    
+    # Add your routes here
+    post "/login" do
+        password_input = params[:password]
+        user_input = params[:username]
+        if (password_input == "1234" && user_input =="Phil")
+                true.to_json 
+        else
+            msg = password_input
+            msg.to_json
+        end
+    end
+end
