fc3ec0f5a180bb44d57e8d3d681853efbf008f6d
Kevin Price <collegekevin@gmail.com>
2022-12-06 12:39:28 -0800

    Got the login to persist. Created custom hook for login.

--- front_end/tokenauth_poc/src/App.js
+++ front_end/tokenauth_poc/src/App.js
@@ -3,10 +3,25 @@ import { Routes, Route } from "react-router-dom"
 import LoginForm from './components/LoginForm/LoginForm';
 import Dashboard from './components/Dashboard/Dashboard';
 import Preferences from './components/Preferences/Preferences';
-import {useState} from 'react'
-function App() {
-  const [token, setToken] = useState(false)
+import useToken from './useToken';
+//import {useState} from 'react'
 
+// function setToken(userToken) {
+//   sessionStorage.setItem('token', JSON.stringify(userToken))
+// }
+// function getToken(){
+//   const tokenString = sessionStorage.getItem('token')
+//   const userToken = JSON.parse(tokenString)
+//   return userToken?.token
+// }
+
+function App() {
+  const {token, setToken} = useToken()
+  //const token = getToken()
+  // if(!token) {
+  //   return <LoginForm setToken={setToken}/>
+  // }
+  // else {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,12 +29,13 @@ function App() {
       {/* {token? null :<LoginForm setToken={setToken} />} */}
       </header>
       <Routes>
-        <Route element={token? <Dashboard/> :<LoginForm setToken={setToken} token={token}/>} path="/dashboard"></Route>
-        <Route element={token? <Preferences/> :<LoginForm setToken={setToken} token={token}/>} path="/preferences"></Route>
+        <Route element={token? <Dashboard/> :<LoginForm setToken={setToken} />} path="/dashboard"></Route>
+        <Route element={token? <Preferences/> :<LoginForm setToken={setToken} />} path="/preferences"></Route>
       </Routes>
       
     </div>
   );
-}
+  }
+
 
 export default App;
