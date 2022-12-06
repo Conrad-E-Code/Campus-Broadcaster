fa8d9e96465ff232879dc3ba8ec81eaa7eab926f
Conrad Etherington <conrad.etherington@gmail.com>
2022-12-05 22:20:46 -0700

    password almost working

--- front_end/tokenauth_poc/src/components/LoginForm/LoginForm.js
+++ front_end/tokenauth_poc/src/components/LoginForm/LoginForm.js
@@ -1,31 +1,66 @@
 import React from "react"
 import './LoginForm.css'
+import {useState} from "react"
+import PropTypes from 'prop-types'
+
+async function loginUser(creds) {
+    return fetch("http://localhost:9292/login",{
+        method:"POST",
+        headers:{"content-type": "application/json"},
+        body: JSON.stringify(creds)
+    })
+    .then( r => r.json())
+}
 
 function LoginForm({setToken}) {
+const [formUser, setFormUser] = useState("")
+const [formPass, SetFormPass] = useState("")
+// const loginObj = {
+//     "username": formUser,
+//     "password": formPass
+// }
 
-    function loginFetch(e) {
+// console.log(loginObj)
+    // async function handleSubmit(e) 
+    const handleSubmit = async e => {
         e.preventDefault()
-        console.log(e)
-        setToken(true)
-        const LOGIN_URL = ""
-        // fetch(LOGIN_URL)
-        // .then(
-        //     console.log("unknown territory")
-        // )
+        const token = await loginUser({
+            "username": formUser,
+            "password": formPass
+        })
+        console.log(token)
+        setToken(token)
     }
+        // console.log(e)
+        // const LOGIN_URL = "changemeurl/login"
+        // console.log(loginObj)
+        // const bpObj = {
+        //     method:"POST",
+        //     headers:{"content-type": "application/json"},
+        //     body: JSON.stringify(loginObj)
+        // }
+
 
     return (
         <div className="login-form-div">
-            <h1> Welcome to The Login Form</h1>
-            <h2> Hello From LoginForm</h2>
-            <form onSubmit={(e) => loginFetch(e)} className="login-form">
+            <h1>You Must Log In To See This Content!</h1>
+            <h2>Enter Credentials Below
+            </h2>
+            <form onSubmit={(e) => handleSubmit(e)} className="login-form">
                 <label> Enter Username:
-                    <input className="login-input" type="text">
+                    <input  onChange={(e) => {
+                        setFormUser(e.target.value)
+                       // console.log(e.target.value)
+                        //console.log(formUser)
+                    }} className="login-input" type="text">
                     </input>
                 </label>
                 <br />
                 <label> Enter Password:
-                    <input className="login-input" type="password">
+                    <input  onChange={(e) => {SetFormPass(e.target.value)
+                    //console.log(e.target.value)
+                    //console.log(formPass)
+                    }} className="login-input" type="password">
                     </input>
                 </label>
                 <br />
@@ -35,5 +70,7 @@ function LoginForm({setToken}) {
         </div>
     )
 }
-
+LoginForm.propTypes = {
+    setToken: PropTypes.func.isRequired
+  }
 export default LoginForm
