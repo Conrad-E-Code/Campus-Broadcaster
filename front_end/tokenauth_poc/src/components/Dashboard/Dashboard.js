0bbff2d0f5b4901302ec39691242227c96280181
oauit <aidanjade.ward@gmail.com>
2022-12-09 10:13:04 -0700

    mess-mess

--- front_end/tokenauth_poc/src/components/Dashboard/Dashboard.js
+++ front_end/tokenauth_poc/src/components/Dashboard/Dashboard.js
@@ -57,10 +57,26 @@ function Dashboard({ token }) {
 
   function handlePrev() {
     // console.log("previous")
-    if (counter < 1000) {
+    if (counter < Infinity) {
       setCounter(counter - 1);
     }
     console.log("previous", counter);
+  }
+  
+  function handleDown() {
+    // console.log("next")
+    if (counter >= 0) {
+      setCounter(counter + 1);
+    }
+    console.log("Down", counter);
+  }
+
+  function handleUp() {
+    // console.log("previous")
+    if (counter < Infinity) {
+      setCounter(counter - 1);
+    }
+    console.log("Up", counter);
   }
 
   return (
@@ -69,9 +85,14 @@ function Dashboard({ token }) {
       <h2>Campus Broadcasts</h2>
       {/* <img alt="pager" src="https://s.isanook.com/an/0/rp/rc/w300h200/yatxacm1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2FuLzAvdmFsZW50aW5lL3N0YXRpYy9pbWFnZXMvMjAyMi9wYWdlci9wYWdlcl8xLnBuZw==.png"></img> */}
       <div className="display-card">
+        
         <button className="previous-post" onClick={handlePrev}></button>
         <br></br>
         <button className="next-post" onClick={handleNext}></button>
+        <br></br>
+        <button className="up-post" onClick={handleUp}></button>
+        <br></br>
+        <button className="down-post" onClick={handleDown}></button>
         <img
           alt="pager"
           src="https://s.isanook.com/an/0/rp/rc/w300h200/yatxacm1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2FuLzAvdmFsZW50aW5lL3N0YXRpYy9pbWFnZXMvMjAyMi9wYWdlci9wYWdlcl8xLnBuZw==.png"
